aaa39143295a9981ad3b0022413479ca4d3fffa1
Jochen Wersdörfer <jochen@wersdoerfer.de>
2014-07-28 16:21:02 +0200

    use installation.sh :/

---
+++ installation.sh
@@ -0,0 +1,8 @@
+export LLVM_CONFIG_PATH=/usr/local/opt/llvm33/bin/llvm-config-3.3
+packages="numpy cython llvmpy numba"
+for package in $packages
+do
+  echo $package
+  pip install $package
+  pip3 install $package
+done
