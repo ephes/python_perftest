0dc977bb2622b8702225aebaf997ba920dbbc103
Jochen Wersdörfer <jochen@wersdoerfer.de>
2014-07-30 15:16:44 +0200

    working list intersection in cython

--- perftest/posting_lists.py
+++ perftest/posting_lists.py
@@ -7,16 +7,23 @@ from numba import jit
 from array import array
 from timeit import Timer
 
-from .posting_lists_cython import intersect_cython
+try:
+    # python 2
+    from itertools import izip
+except ImportError as err:
+    # python 3
+    izip = zip
 
-def get_random_ints(sample_size=50000, a=1, b=500000):
+from .posting_lists_cython import intersect_2cython
+from .posting_lists_cython import intersect_lists_cython
+
+def get_random_ints(sample_size=5000, a=1, b=500000):
     return sorted(random.sample(range(b), sample_size))
 
 
 class IntersectBase(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, lists):
+        self.lists = lists
 
     def timeit(self):
         t = Timer(self)
@@ -27,20 +34,24 @@ class PythonListIntersect(IntersectBase):
     timeit_string = "naive python list intersection    : {:>30}s"
 
     def __call__(self):
-        x, y = self.x, self.y
-        p1, p2 = 0, 0
+        lists = self.lists
+        pointers = [0 for l in lists]
         intersection = []
-        max_x, max_y = len(x), len(y)
-        while p1 < max_x and p2 < max_y:
-            a, b = x[p1], y[p2]
-            if a == b:
-                intersection.append(a)
-                p1 += 1
-                p2 += 1
-            elif a < b:
-                p1 += 1
+        list_lens = [len(l) for l in lists]
+        not_finished = len([(i, j) for i, j in zip(pointers, list_lens) if i >= j]) == 0
+        while not_finished:
+            values = [l[p] for l, p in izip(lists, pointers)]
+            if len(set(values)) == 1:
+                intersection.append(values[0])
+                for i, p in enumerate(pointers):
+                    pointers[i] += 1
+                    if pointers[i] >= list_lens[i]:
+                        not_finished = False
             else:
-                p2 += 1
+                min_val, min_num = min([(j, i) for i, j in enumerate(values)])
+                pointers[min_num] += 1
+                if pointers[min_num] >= list_lens[min_num]:
+                    not_finished = False
         self.intersection = intersection
         return intersection
 
@@ -73,58 +84,74 @@ class NumbaListIntersect(IntersectBase):
     timeit_string = "python list intersection with @jit: {:>30}s"
 
     def __call__(self):
-        self.intersection = numba_list_intersect(self.x, self.y)
+        lists = self.lists
+        tmp_intersection = numba_list_intersect(lists[0], lists[1])
+        for new_list in lists[2:]:
+            tmp_intersection = numba_list_intersect(tmp_intersection, new_list)
+        self.intersection = tmp_intersection
         return self.intersection
 
 
 class NumpyArrayIntersect(IntersectBase):
     timeit_string = "numpy array intersection          : {:>30}s"
     
-    def __init__(self, x, y):
-        super(NumpyArrayIntersect, self).__init__(x, y)
-        self.xa = np.array(self.x)
-        self.ya = np.array(self.y)
+    def __init__(self, lists):
+        super(NumpyArrayIntersect, self).__init__(lists)
+        self.array_lists = []
+        for posting_list in self.lists:
+            self.array_lists.append(np.array(posting_list))
 
     def __call__(self):
-        self.intersection = np.intersect1d(self.xa, self.ya)
+        tmp_intersection = np.intersect1d(self.array_lists[0], self.array_lists[1])
+        for new_list in self.array_lists[2:]:
+            tmp_intersection = np.intersect1d(tmp_intersection, new_list)
+        self.intersection = tmp_intersection
         return self.intersection
 
 
 class PythonSetIntersect(IntersectBase):
     timeit_string = "python set intersection           : {:>30}s"
     
-    def __init__(self, x, y):
-        super(PythonSetIntersect, self).__init__(x, y)
-        self.xs = set(self.x)
-        self.ys = set(self.y)
+    def __init__(self, lists):
+        super(PythonSetIntersect, self).__init__(lists)
+        self.set_lists = []
+        for posting_list in self.lists:
+            self.set_lists.append(set(posting_list))
     
     def __call__(self):
-        self.intersection = self.xs.intersection(self.ys)
+        self.intersection = set.intersection(*self.set_lists)
+        #print(sorted(list(self.intersection)))
+        return self.intersection
 
 
 class PythonArrayIntersect(IntersectBase):
     timeit_string = "python array intersection         : {:>30}s"
 
-    def __init__(self, x, y):
-        super(PythonArrayIntersect, self).__init__(x, y)
-        self.xar = array("i", self.x)
-        self.yar = array("i", self.y)
+    def __init__(self, lists):
+        super(PythonArrayIntersect, self).__init__(lists)
+        self.array_lists = []
+        for posting_list in self.lists:
+            self.array_lists.append(array("i", posting_list))
 
     def __call__(self):
-        x, y = self.xar, self.yar
-        p1, p2 = 0, 0
+        lists = self.array_lists
+        pointers = array("i", [0 for l in lists])
         intersection = array("i")
-        max_x, max_y = len(x), len(y)
-        while p1 < max_x and p2 < max_y:
-            a, b = x[p1], y[p2]
-            if a == b:
-                intersection.append(a)
-                p1 += 1
-                p2 += 1
-            elif a < b:
-                p1 += 1
+        array_lens = [len(ar) for ar in lists]
+        not_finished = len([(i, j) for i, j in zip(pointers, array_lens) if i >= j]) == 0
+        while not_finished:
+            values = [l[p] for l, p in izip(lists, pointers)]
+            if len(set(values)) == 1:
+                intersection.append(values[0])
+                for i, p in enumerate(pointers):
+                    pointers[i] += 1
+                    if pointers[i] >= array_lens[i]:
+                        not_finished = False
             else:
-                p2 += 1
+                min_val, min_num = min([(j, i) for i, j in enumerate(values)])
+                pointers[min_num] += 1
+                if pointers[min_num] >= array_lens[min_num]:
+                    not_finished = False
         self.intersection = intersection
         return intersection
 
@@ -132,18 +159,21 @@ class PythonArrayIntersect(IntersectBase):
 class CythonArrayIntersect(IntersectBase):
     timeit_string = "cython array intersection         : {:>30}s"
 
-    def __init__(self, x, y):
-        super(CythonArrayIntersect, self).__init__(x, y)
-        self.xar = array("i", self.x)
-        self.yar = array("i", self.y)
-
+    def __init__(self, lists):
+        super(CythonArrayIntersect, self).__init__(lists)
+        self.array_lists = []
+        for posting_list in self.lists:
+            self.array_lists.append(array("i", posting_list))
+        
     def __call__(self):
-        self.intersection = intersect_cython(self.xar, self.yar)
+        #self.intersection = intersect_2cython(self.xar, self.yar)
+        self.intersection = intersect_lists_cython(self.array_lists)
         return self.intersection
     
 
 def run_timeit():
-    x, y = get_random_ints(), get_random_ints()
+    #x, y, z = get_random_ints(), get_random_ints(), get_random_ints()
+    posting_lists = [get_random_ints() for i in range(5)]
     test_classes = [
         NumbaListIntersect,
         PythonArrayIntersect,
@@ -153,5 +183,5 @@ def run_timeit():
         CythonArrayIntersect,
     ]
     for TestClass in  test_classes:
-        test_instance = TestClass(x, y)
+        test_instance = TestClass(posting_lists)
         test_instance.timeit()
--- perftest/posting_lists_cython.pyx
+++ perftest/posting_lists_cython.pyx
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+cimport cython
 
 from cpython cimport array
 from array import array
 
-cpdef array.array intersect_cython(object x, object y):
+from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+
+cpdef array.array intersect_2cython(object x, object y):
     cdef int[:] cx = x
     cdef int[:] cy = y
     cdef int p1, p2
@@ -20,8 +23,10 @@ cpdef array.array intersect_cython(object x, object y):
     is_size = max(max_x, max_y)
     intersection = array.clone(int_array_template, is_size, zero=False)
 
+    cdef int counter
     cdef int a, b
     while p1 < max_x and p2 < max_y:
+        counter += 1
         a, b = cx[p1], cy[p2]
         if a == b:
             intersection[is_idx] = a
@@ -33,4 +38,73 @@ cpdef array.array intersect_cython(object x, object y):
         else:
             p2 += 1 
     array.resize(intersection, is_idx)
+    return intersection
+
+cpdef array.array intersect_lists_cython(lists):
+    cdef int lists_len = len(lists)
+
+    cdef int **my_arrays = <int **>PyMem_Malloc(lists_len * cython.sizeof(int))
+    if not my_arrays:
+        raise MemoryError()
+
+    cdef int *pointers = <int *>PyMem_Malloc(lists_len * cython.sizeof(int))
+    if not pointers:
+        raise MemoryError()
+
+    cdef int *list_lens = <int *>PyMem_Malloc(lists_len * cython.sizeof(int))
+    if not list_lens:
+        raise MemoryError()
+
+    cdef array.array int_array_template = array('i', [])
+    cdef array.array intersection
+    cdef int intersection_idx, max_size
+    intersection_idx, max_size = 0, -1
+
+    cdef int[:] tmp_array
+
+    for i in range(lists_len):
+        tmp_array = lists[i]
+        my_arrays[i] = &tmp_array[0]
+        pointers[i] = 0
+        list_lens[i] = len(lists[i])
+        if list_lens[i] > max_size:
+            max_size = list_lens[i]
+
+    intersection = array.clone(int_array_template, max_size, zero=False)
+
+    cdef int min_val, min_idx, tmp_val, prev_val, all_same
+    min_val, min_idx, tmp_val, prev_val, all_same = -1, -1, -1, -1, -1
+
+    cdef int not_finished = 1
+    while not_finished:
+        all_same = 1
+        for i in range(lists_len):
+            tmp_val = my_arrays[i][pointers[i]]
+            if i > 0:
+                if tmp_val < min_val:
+                    min_val = tmp_val
+                    min_idx = i
+                    all_same = 0
+                elif tmp_val > min_val:
+                    all_same = 0
+            else:
+                min_val = tmp_val
+                min_idx = i
+        if all_same == 1:
+            intersection[intersection_idx] = min_val
+            intersection_idx += 1
+            for i in range(lists_len):
+                pointers[i] += 1
+                if pointers[i] >= list_lens[i]:
+                    not_finished = 0
+        else:
+            pointers[min_idx] += 1
+            if pointers[min_idx] >= list_lens[min_idx]:
+                not_finished = 0
+
+    PyMem_Free(my_arrays)
+    PyMem_Free(pointers)
+    PyMem_Free(list_lens)
+
+    array.resize(intersection, intersection_idx)
     return intersection
