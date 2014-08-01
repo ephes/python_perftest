a8753860f9f3b602c9d941bc60137df01f25a19c
Jochen Wersdörfer <jochen@wersdoerfer.de>
2014-08-01 10:09:27 +0200

    let setup.py do its job

--- python2.sh
+++ python2.sh
@@ -1,6 +1,3 @@
-rm *.so
 rm perftest/*.so
 rm perftest/perftest/posting_lists_cython.c
 time python setup.py build_ext --inplace
-cp posting_lists_cython.so perftest
-cp posting_lists_extension.so perftest
